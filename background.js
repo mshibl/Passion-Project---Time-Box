d299f7576b2407973cd3416798184d6cb5f5915d
mshibl <mohamed.shibl@hotmail.com>
2015-11-16 19:28:07 -0800

    adds changes to background.js

--- background.js
+++ background.js
@@ -2,21 +2,43 @@ var timerProcess;
 var startTime;
 var timeLimit;
 var timePassed;
+var breakTimerOn = false;
 
 
-function timer(){
-  timePassed = (Date.now() - startTime) / 1000
-  if (timePassed > timeLimit) {
-    console.log('timer done!')
-    chrome.storage.sync.set({'timerRunning': false}, function() {
-      // This is the alert that will be displayed to the user once the timer is done
-      // alert('Time Limit Reached! \n Take a break!')
-      confirm('Time Limit Reached!\nTimer will restart after the ' + breakLimit/60 + ' minutes break\n\nClick cancel if you wish to stop ..')
-      // notification.show();
-      clearInterval(timerProcess);
-    })
-  }
+function timerReset(){
+  startTime = Date.now();
 }
+
+function notification(){
+  var notification = confirm('Time Limit Reached!\nTimer will restart after the ' + breakLimit/60 + ' minutes break\n\nChoose cancel if you wish to stop ..');
+  if (notification == true){
+
+  } else {
+    chrome.storage.sync.set({'timerRunning': false})
+  }
+
+}
+
+// function timer(){
+//   timePassed = (Date.now() - startTime) / 1000
+//   if (timePassed > timeLimit) {
+//     if (breakTimerOn){
+//       breakTimerOn = false;
+
+//     }
+//     console.log('timer done!')
+//     chrome.storage.sync.set({'timerRunning': false}, function() {
+//       // This is the alert that will be displayed to the user once the timer is done
+//       var notification = confirm('Time Limit Reached!\nTimer will restart after the ' + breakLimit/60 + ' minutes break\n\nChoose cancel if you wish to stop ..')
+//       if (notification == true){
+//         breakTimerOn = true;
+//         setTimeout(timerReset,breakLimit);
+//       } else {
+//         clearInterval(timerProcess);
+//       }
+//     })
+//   }
+// }
 
 // Connection from popup.js when user starts the timer
 chrome.extension.onConnect.addListener(function(port){
@@ -29,9 +51,9 @@ chrome.extension.onConnect.addListener(function(port){
       default:
         breakLimit = msg[1] * 60
         timeLimit = msg[0] * 60
-        startTime = Date.now()
+        // startTime = Date.now()
         chrome.storage.sync.set({'timerRunning': true, 'timeLimit': timeLimit, 'startTime': startTime})
-        timerProcess = setInterval(timer, 1000)
+        timerProcess = setTimeout(notification, (timeLimit * 1000))
     }
   })
 })
