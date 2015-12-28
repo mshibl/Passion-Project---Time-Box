27050c3781413f304461a69d3dadbba12bceb037
mshibl <mohamed.shibl@hotmail.com>
2015-12-28 00:59:48 -0800

    organizes all icons into one folder and adds button to notification

--- background.js
+++ background.js
@@ -13,26 +13,31 @@ function timerReset(){
 }
 
 function notification(){
-  var notification = new Notification('Notification title', {
-    icon: '48PX.png',
-    title: 'Time Limit Reached!'
-    body: 'Timer will restart after the ' + breakLimit/60 + ' minutes break',
-    // buttons: [{
-    //   title: 'Test Button',
-    //   icon: '16PX.png'
-    // }]
-  });
+var notification=chrome.notifications.create("", {
+    type:    "basic",
+    iconUrl: "icons/48PX.png",
+    title:   "Time Limit Reached",
+    // message: 'Timer will restart after the ' + breakLimit/60 + ' minutes break',
+    // contextMessage: "It's about time...",
+    contextMessage: 'Timer will restart after the ' + breakLimit/60 + ' minutes break',
+    buttons: [{
+        title: "Stop the timer",
+        iconUrl: "icons/stopButton.png"
+    }]
+   }, function(id) {
+    myNotificationID = id;
+});
   // var notification = confirm('Time Limit Reached!\nTimer will restart after the ' + breakLimit/60 + ' minutes break\n\nChoose cancel if you wish to stop ..');
-  if (notification == true){
+  // if (notification == true){
     chrome.storage.sync.set({'timeLimit': breakLimit , 'startTime': Date.now(), 'breakTime': true})
     setTimeout(function(){
       chrome.storage.sync.set({'timeLimit': timeLimit , 'startTime': Date.now(), 'breakTime': false});
       alert('Break time is over! \n Time Box will restart now');
       initTimer();
     }, (breakLimit * 1000));
-  } else {
-    chrome.storage.sync.set({'timerRunning': false})
-  }
+  // } else {
+  //   chrome.storage.sync.set({'timerRunning': false})
+  // }
 
 }
 
